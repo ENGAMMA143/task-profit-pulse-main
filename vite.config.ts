abe6488526dd2104172fca61164e29c89a10ca49
Abdulrahman Almiqdad <engabdulrhman143@gmail.com>
2025-07-02 19:54:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    // أضف هنا أي إضافات أخرى لطور التطوير فقط
-    // mode === 'development' && yourDevPlugin(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          supabase: ['@supabase/supabase-js'],
+          binance: ['@binance/connector']
+        }
+      }
     },
-  },
-}));
+    chunkSizeWarningLimit: 1000
+  }
+});
