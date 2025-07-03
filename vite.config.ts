7b605c1adeece41f679e9d3ec1889d0bd5388e4f
Abdulrahman Almiqdad <engabdulrhman143@gmail.com>
2025-07-03 09:40:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // ➡️ هنا أضف resolve.alias
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-
   build: {
     rollupOptions: {
       output: {
@@ -21,5 +25,11 @@ export default defineConfig({
       }
     },
     chunkSizeWarningLimit: 1000
+  },
+  define: {
+    'process.env': {
+      VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL),
+      BINANCE_API_KEY: JSON.stringify(process.env.BINANCE_API_KEY)
+    }
   }
 });
